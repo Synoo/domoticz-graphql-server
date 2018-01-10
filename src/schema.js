77fe0abab7797be64b5c58ebebe9f6f40d1d852b
synoo <patrick.buijs33@gmail.com>
2018-01-10 14:40:53 +0100

    Refactor with graphql-tools and add arguments interval

--- src/schema.js
+++ src/schema.js
@@ -1,69 +1,48 @@
 const { GraphQLString, GraphQLFloat, GraphQLList, GraphQLObjectType, GraphQLNonNull, GraphQLSchema } = require('graphql');
 const axios = require('axios');
 const _ = require('lodash');
+const { makeExecutableSchema } = require('graphql-tools');
 
-const LigthSwitchType = new GraphQLObjectType({
-    name: "LightSwitch",
-    description: "This represents an lightSwitch",
-    fields: () => ({
-      Name: {type: new GraphQLNonNull(GraphQLString)},
-      idx: {type: new GraphQLNonNull(GraphQLString)}
+const typeDefs = `
+  type LightSwitch {
+    idx: Int!,
+    Name: String
+  }
+
+  type TemperatureGas {
+    d: String!,
+    tm: Float,
+    te: Float,
+    ta: Float,
+    gas(interval: String): Float
+  }
+
+  type Query {
+    allLightSwitches: [LightSwitch],
+    temperatureGas(interval: String): [TemperatureGas]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    allLightSwitches: () => axios.get('http://synoo:synoo@192.168.178.101:8080/json.htm?type=command&param=getlightswitches').then(function (response) {
+      return response.data.result
+    }),
+    temperatureGas: (_, {interval}) => axios.get('http://synoo:synoo@192.168.178.101:8080/json.htm?type=graph&sensor=temp&idx=29&range=' + interval).then(function (response) {
+      return response.data.result
     })
-});
-
-const TemperatureGasType = new GraphQLObjectType({
-  name: "TemperatureGasType",
-  description: "This represents an TemperatureGas",
-  fields: () => ({
-    d: {type: new GraphQLNonNull(GraphQLString)},
-    tm: {type: new GraphQLNonNull(GraphQLFloat)},
-    ta: {type: new GraphQLNonNull(GraphQLFloat)},
-    te: {type: new GraphQLNonNull(GraphQLFloat)},
-    gas: {
-      type: GraphQLString,
-      resolve: function(temp) {
-        const gasMonthly = axios.get('http://synoo:synoo@192.168.178.101:8080/json.htm?type=graph&sensor=counter&idx=7&range=month').then(function (response) {
-              return _.find(response.data.result, { 'd': temp.d}).v
-            })
-
-        return gasMonthly;
-      }
-    }
-  })
-});
-
-const RootType = new GraphQLObjectType({
-    name: 'RootTypeSchema',
-    description: "RootTypeSchema",
-    fields: () => ({
-      allLightSwitches: {
-        type: new GraphQLList(LigthSwitchType),
-        description: "List of all Lightswitches",
-        resolve: function() {
-            const lightSwitches = axios.get('http://synoo:synoo@192.168.178.101:8080/json.htm?type=command&param=getlightswitches').then(function (response) {
-                return response.data.result
-            })
-
-            return lightSwitches;
-        }
-      },
-      temperatureGasMonthly: {
-        type: new GraphQLList(TemperatureGasType),
-        description: "List of monthly TemperatureGasType",
-        resolve: function() {
-            const temperatureGasMonthly = axios.get('http://synoo:synoo@192.168.178.101:8080/json.htm?type=graph&sensor=temp&idx=29&range=month').then(function (response) {
-                return response.data.result
-            })
-  
-            return temperatureGasMonthly;
-        }
-      }
+  },
+  TemperatureGas: {
+    gas: (temp, {interval}) => axios.get('http://synoo:synoo@192.168.178.101:8080/json.htm?type=graph&sensor=counter&idx=7&range=' + interval).then(function (response) {
+      return _.find(response.data.result, { 'd': temp.d}).v
     })
-  });
+  }
+}
 
-const Schema = new GraphQLSchema({
-    query: RootType
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
 });
 
-module.exports = Schema;
+module.exports = schema;
 
